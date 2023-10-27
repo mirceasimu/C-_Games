09d6cd27b269510ea984edf648f4dc982cf277e7
Mircea Simu <mirceasimu6@gmail.com>
2023-10-28 00:07:31 +0200

    Add files via upload
    A minesweeper game with a few bugs that was made as a prototype before my actual working try which I can not find..

---
+++ Minesweeper_Simu_Mircea - Copy/Minesweeper_Simu_Mircea/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Minesweeper_Simu_Mircea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_gri = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.start_verde = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.start_gri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_gri
+            // 
+            this.start_gri.Image = global::Minesweeper_Simu_Mircea.Properties.Resources.start_minesweeper_gri;
+            this.start_gri.Location = new System.Drawing.Point(204, 8);
+            this.start_gri.Name = "start_gri";
+            this.start_gri.Size = new System.Drawing.Size(100, 31);
+            this.start_gri.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.start_gri.TabIndex = 1;
+            this.start_gri.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(99, 56);
+            this.listBox1.TabIndex = 2;
+            // 
+            // start_verde
+            // 
+            this.start_verde.BackgroundImage = global::Minesweeper_Simu_Mircea.Properties.Resources.start_minesweeper_verde;
+            this.start_verde.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.start_verde.Location = new System.Drawing.Point(204, 8);
+            this.start_verde.Name = "start_verde";
+            this.start_verde.Size = new System.Drawing.Size(100, 31);
+            this.start_verde.TabIndex = 3;
+            this.start_verde.UseVisualStyleBackColor = true;
+            this.start_verde.Click += new System.EventHandler(this.start_verde_Click_1);
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(12, 12);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 4;
+            this.show.Text = "button1";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Minesweeper_Simu_Mircea.Properties.Resources.minesweeper_format;
+            this.ClientSize = new System.Drawing.Size(496, 314);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.start_verde);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.start_gri);
+            this.Name = "Form1";
+            this.Text = "Minesweeper";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.start_gri)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox start_gri;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button start_verde;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Minesweeper_Simu_Mircea - Copy/Minesweeper_Simu_Mircea/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper_Simu_Mircea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[,] m = new int[16, 30] { {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+                                     {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }};
+
+        int l;
+        int c;
+        int l1, c1;
+        Random r = new Random();
+        Point current = new Point();
+
+        private void start_verde_Click_1(object sender, EventArgs e)
+        {
+            int bombe = 0;
+            int bombemax = 90;
+            //l = r.Next(0, 16);
+            //c = r.Next(0, 30);
+            //l1 = l;
+            //c1 = c;
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 30; j++)
+            //    {
+            //        l = new Random().Next(0, 16);
+            //        c = new Random().Next(0, 30);
+            //        if (l != l1 || c != c1)
+            //        {
+            //            l1 = l;
+            //            c1 = c;
+            //            listBox1.Items.Add(l + " " + c);
+            //            int aux;
+            //            aux = m[i, j];
+            //            m[i, j] = m[l, c];
+            //            m[l, c] = aux;
+            //        }
+            //        else
+            //            j--;
+            //    }
+            //}
+
+            do
+            {
+                bombe++;
+                do
+                {
+                    l = r.Next(0, 16);
+                    c = r.Next(0, 30);
+                } while (m[l, c] != 0);
+                m[l, c] = -1;
+            } while (bombe <= bombemax);
+
+            start_verde.Visible = false;
+            // pana aici se face matricea cu 0 si -1 random puși
+
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    if(m[i,j]==-1)
+                    {
+                        for(int v1=-1;v1<=1;v1++)
+                            for(int v2=-1;v2<=1;v2++)
+                            {
+                                if(i+v1 >=0 && i+v1 <16 && j+v2<30 && j+v2>=0)
+                                    if (m[i + v1, j + v2] != -1)
+                                        m[i + v1, j + v2]++;
+                            }
+                    }
+                }
+            }
+        }
+
+        void parcurgere(int i, int j, PictureBox pic)
+        {
+            if(i>=0 && i<16 && j>=0 && j<30)
+            {
+                if (m[i, j] == 0)
+                {
+                    pic.Image = Properties.Resources._0;
+                    pic.Top = i * 16 + 50;
+                    pic.Left = j * 16 + 8;
+                    pic.Visible = true;
+                    pic.BringToFront();
+                    pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                    this.Controls.Add(pic);
+                    PictureBox pic1 = new PictureBox();
+                    parcurgere(i, j+1, pic1);
+                    //parcurgere(i, j-1, pic1);
+                    //parcurgere(i+1, j);
+                    //parcurgere(i-1, j);
+                }
+                else
+                    if (m[i, j] > 0)
+                {
+                    if (m[i, j] == 1)
+                        pic.Image = Properties.Resources._1;
+                    if (m[i, j] == 2)
+                        pic.Image = Properties.Resources._2;
+                    if (m[i, j] == 3)
+                        pic.Image = Properties.Resources._3;
+                    if (m[i, j] == 4)
+                        pic.Image = Properties.Resources._4;
+                    if (m[i, j] == 5)
+                        pic.Image = Properties.Resources._5;
+                    pic.Top = i * 16 + 50;
+                    pic.Left = j * 16 + 8;
+                    pic.Visible = true;
+                    pic.BringToFront();
+                    pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                    this.Controls.Add(pic);
+                }
+                else
+                    if (m[i, j] == -1)
+                {
+                    pic.Image = Properties.Resources.bomba_nelovita;
+                    pic.Top = i * 16 + 50;
+                    pic.Left = j * 16 + 8;
+                    pic.Visible = true;
+                    pic.BringToFront();
+                    pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                    this.Controls.Add(pic);
+                }
+            }
+        }
+
+    private void Form1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            current = me.Location;
+            int mx=me.X, my=me.Y;
+            mx = (mx - 8) / 16;
+            my = (my - 50) / 16;
+            if(me.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                /*foreach(Control x in this.Controls)
+                {
+                    if((string)x.Tag=="steag")
+                    {
+                        x.Dispose();
+                    }
+                }*/
+                PictureBox p = new PictureBox();
+                p.Image = Properties.Resources.steag;
+                p.Top = my * 16 + 50;
+                p.Left = mx * 16 + 8;
+                p.Visible = true;
+                p.Tag = "steag";
+                p.BringToFront();
+                p.SizeMode = PictureBoxSizeMode.AutoSize;
+                this.Controls.Add(p);
+            }
+            if (me.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                PictureBox p = new PictureBox();
+                if (m[my, mx] == 0)
+                {
+                    parcurgere(my, mx, p);
+                    //p.Image = Properties.Resources._0;
+                }
+                if (m[my, mx] == 1)
+                    p.Image = Properties.Resources._1;
+                if (m[my, mx] == 2)
+                    p.Image = Properties.Resources._2;
+                if (m[my, mx] == 3)
+                    p.Image = Properties.Resources._3;
+                if (m[my, mx] == 4)
+                    p.Image = Properties.Resources._4;
+                if (m[my, mx] == 5)
+                    p.Image = Properties.Resources._5;
+                p.Top = my * 16 + 50;
+                p.Left = mx * 16 + 8;
+                p.Visible = true;
+                p.BringToFront();
+                p.SizeMode = PictureBoxSizeMode.AutoSize;
+                this.Controls.Add(p);
+                if (m[my, mx] == -1)
+                {
+                    p.Image = Properties.Resources.bomba_lovita;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i != 16; i++)
+            {
+                listBox1.Items.Add(m[i, 0] + " " + m[i, 1] + " " + m[i, 2] + " " + m[i, 3] + " " + m[i, 4] + " " + m[i, 5] + " " + m[i, 6] + " " + m[i, 7] + " " + m[i, 8] + " " + m[i, 9] + " " + m[i, 10] + " " +
+                                    m[i, 11] + " " + m[i, 12] + " " + m[i, 13] + " " + m[i, 14] + " " + m[i, 15] + " " + m[i, 16] + " " + m[i, 17] + " " + m[i, 18] + " " + m[i, 19] + " " + m[i, 20] + " " +
+                                    m[i, 21] + " " + m[i, 22] + " " + m[i, 23] + " " + m[i, 24] + " " + m[i, 25] + " " + m[i, 26] + " " + m[i, 27] + " " + m[i, 28] + " " + m[i, 29]);
+            }
+        }
+    }
+}
